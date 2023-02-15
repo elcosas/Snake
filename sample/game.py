fa164d380fe94a29c764f6ddf046f9d207168ee9
elcosas <121999809+elcosas@users.noreply.github.com>
2023-02-14 21:41:16 -0800

    completed Apple class and add snake collisions

--- sample/game.py
+++ sample/game.py
@@ -1,6 +1,6 @@
-import random
 import pygame
 from pygame.locals import *
+from random import choice
 
 BLOCKSIZE = 40
 
@@ -10,8 +10,10 @@ def main():
     grid = [[None for y in range(0, 600 // BLOCKSIZE)] for x in range(0, 800 // BLOCKSIZE)]
 
     SPAWNAPPLE = pygame.USEREVENT + 1
+    spawnapple_evt = pygame.event.Event(SPAWNAPPLE)
+    pygame.event.post(spawnapple_evt)
 
-    window = pygame.display.set_mode((800, 600)) 
+    window = pygame.display.set_mode((800, 600))
     player = Snake((400, 300))
 
     running = True
@@ -20,9 +22,15 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+            elif event.type == SPAWNAPPLE:
+                new_x = choice(range(0, 800 // BLOCKSIZE))
+                new_y = choice(range(0, 600 // BLOCKSIZE))
+                apple = Apple((new_x, new_y))
+                grid[new_x][new_y] = apple
+
         player.get_input(pygame.key.get_pressed())
         window.fill((0, 0, 0))
-        player.move_snake(grid)
+        player.move_snake(grid, apple, spawnapple_evt)
         draw_grid(grid, window)
         pygame.display.flip()
 
@@ -35,6 +43,8 @@ def draw_grid(grid, window):
             if type(row) == pygame.Rect:
                 seg = row.copy().move(row.x*BLOCKSIZE - row.x, row.y*BLOCKSIZE - row.y)
                 pygame.draw.rect(window, (0, 255, 0), seg)
+            elif type(row) == Apple:
+                window.blit(row.surface, row.rect)
             else:
                 pygame.draw.rect(window, (255, 255, 255), pygame.Rect(i*BLOCKSIZE, j*BLOCKSIZE, BLOCKSIZE, BLOCKSIZE), 1)
 
@@ -58,7 +68,7 @@ class Snake(pygame.sprite.Sprite):
         elif pressed_keys[K_RIGHT]:
             self.dir = [1, 0]
 
-    def move_snake(self, grid):
+    def move_snake(self, grid, active_apl, events):
         for i in range(len(self.segments)-1, 0, -1):
             if i == len(self.segments)-1:
                 grid[self.segments[i].x][self.segments[i].y] = None
@@ -67,17 +77,30 @@ class Snake(pygame.sprite.Sprite):
             self.segments[i].move_ip(new_x, new_y)
             grid[self.segments[i].x][self.segments[i].y] = self.segments[i]
         head = self.segments[0]
-        head.move_ip(self.dir[0], self.dir[1])
+        if not self.__collision(grid, active_apl, events):
+            head.move_ip(self.dir[0], self.dir[1])
         grid[head.x][head.y] = head
+
+    def __collision(self, grid, active_apl, events):
+        future_point = [self.segments[0].x + self.dir[0], self.segments[1].y + self.dir[1]]
+        if grid[future_point[0]][future_point[1]] in self.segments:
+            # TODO: Call Game Over
+            pygame.quit()
+            return True
+        elif type(grid[future_point[0]][future_point[1]]) == Apple:
+            active_apl.kill()
+            pygame.event.post(events)
+        return False
 
 class Apple(pygame.sprite.Sprite):
     """Apple that can be consumed by snake, spawns
     randomly on the grid"""
+    SCORE = 1
     def __init__(self, coords):
         super(Apple, self).__init__()
-        self.surface = pygame.Surface(BLOCKSIZE, BLOCKSIZE)
+        self.surface = pygame.Surface((BLOCKSIZE, BLOCKSIZE))
         self.surface.fill((255, 0, 0))
-        self.rect = self.surface.get_rect((coords[0] // BLOCKSIZE, coords[1] // BLOCKSIZE))
+        self.rect = self.surface.get_rect(topleft=(coords[0]*BLOCKSIZE, coords[1]*BLOCKSIZE))
 
 if __name__ == '__main__':
     main()
